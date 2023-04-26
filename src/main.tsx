a47f53425ac1a00a5aee528cd062f0ca2ed63640
Dimitry-prog <kfifa3@gmail.com>
2023-04-26 17:05:53 +0300

    fix: basename

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import { BrowserRouter } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/test-antipoff/">
         <App/>
       </BrowserRouter>
     </Provider>
